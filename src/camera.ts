9c3f268b67651a27b57eb3937009518f36ba152e
DahTach <francesco.tacinelli@gmail.com>
2025-01-09 11:59:45 +0100

    feat(disconnection listener and improvements)
    added disconnection webUSB improvements
    added existing device connection automation

--- src/camera.ts
+++ src/camera.ts
@@ -1,36 +1,39 @@
 /// <reference types="w3c-web-usb" />
-import { initModule, Context, Module } from './libapi';
-import { BehaviorSubject } from 'rxjs';
+import { initModule, Context, Module } from "./libapi";
+import { BehaviorSubject } from "rxjs";
 
 export enum CameraState {
   BUSY,
   READY,
   CONNECTED,
   DISCONNECTED,
-  ERROR
+  ERROR,
 }
 
 const INTERFACE_CLASS = 6; // PTP
 const INTERFACE_SUBCLASS = 1; // MTP
-// const CANON_PID = 13026;
+const CANON_PID = 13026;
 // TEST: Nikon D3200
-const CANON_PID = 1068;
+// const CANON_PID = 1068;
 
-let ModulePromise: Promise<Module>
+let ModulePromise: Promise<Module>;
 
 export class Camera {
-
   #queue: Promise<any> = Promise.resolve();
   #context: Context | null = null;
 
   private _state = new BehaviorSubject<CameraState>(CameraState.DISCONNECTED);
   public state = this._state.asObservable();
 
-  private static device: USBDevice | null = null
+  private static device: USBDevice | null = null;
 
   constructor() {
-    window.addEventListener('beforeunload', async () => {
+    window.addEventListener("beforeunload", async () => {
       this.disconnect();
+    });
+
+    navigator.usb.addEventListener("disconnect", (event) => {
+      this._state.next(CameraState.DISCONNECTED);
     });
   }
 
@@ -38,7 +41,7 @@ export class Camera {
     await Promise.all([
       camera.open(),
       camera.selectConfiguration(1),
-      camera.claimInterface(0)
+      camera.claimInterface(0),
     ]);
   }
 
@@ -46,43 +49,52 @@ export class Camera {
     try {
       // Return cached device if available
       if (Camera.device?.opened) {
-        console.log('%c camera already open', 'color: yellow;', Camera.device);
+        console.log("%c camera already open", "color: yellow;", Camera.device);
         return Camera.device;
       }
 
       // Check existing devices
       const devices = await navigator.usb.getDevices();
-      console.log('%c found devices:', 'color: blue;', devices, '\n looking for', CANON_PID);
-      const existingCamera = devices.find(device => device.productId === CANON_PID);
+      console.log(
+        "%c found devices:",
+        "color: blue;",
+        devices,
+        "\n looking for",
+        CANON_PID,
+      );
+      const existingCamera = devices.find(
+        (device) => device.productId === CANON_PID,
+      );
 
       if (existingCamera) {
         // TODO:
-        // try {
-        //   await this.initializeCamera(existingCamera);
-        // } catch (error) {
-        //   await existingCamera.releaseInterface(0)
-        //   await existingCamera.reset();
-        //   console.log('%c camera reset', 'color: yellow;', existingCamera);
-        //   await this.initializeCamera(existingCamera);
-        // }
+        try {
+          await this.initializeCamera(existingCamera);
+        } catch (error) {
+          await existingCamera.releaseInterface(0);
+          await existingCamera.reset();
+          console.log("%c camera reset", "color: yellow;", existingCamera);
+          await this.initializeCamera(existingCamera);
+        }
         Camera.device = existingCamera;
-        console.log('%c found paired camera', 'color: green;', existingCamera);
+        console.log("%c found paired camera", "color: green;", existingCamera);
         return existingCamera;
       }
 
       // If no camera found, request new device
       const newDevice = await navigator.usb.requestDevice({
-        filters: [{
-          classCode: INTERFACE_CLASS,
-          subclassCode: INTERFACE_SUBCLASS
-        }]
+        filters: [
+          {
+            classCode: INTERFACE_CLASS,
+            subclassCode: INTERFACE_SUBCLASS,
+          },
+        ],
       });
 
       Camera.device = newDevice;
       return newDevice;
-
     } catch (error) {
-      console.error('Camera pairing failed:', error);
+      console.error("Camera pairing failed:", error);
       Camera.device = null;
       return null;
     }
@@ -102,29 +114,37 @@ export class Camera {
 
   // TODO:
   private async attemptRecovery() {
-    this.disconnect().then(() => { })
+    this.disconnect().then(() => {});
   }
 
   private async loadWASM(): Promise<void> {
-    if (this.#context) return
+    if (this.#context) return;
 
     try {
       if (!ModulePromise) {
         ModulePromise = initModule();
       }
 
-      console.log('%c loading Wasm MODULE ...', 'color: blue;');
+      console.log("%c loading Wasm MODULE ...", "color: blue;");
       let Module: Module = await ModulePromise;
-      console.log('%c gPhoto2 Wasm Module Loaded:', 'color: green;', Module);
+      console.log("%c gPhoto2 Wasm Module Loaded:", "color: green;", Module);
 
-      console.log('%c loading Wasm CONTEXT ...', 'color: blue;');
+      console.log("%c loading Wasm CONTEXT ...", "color: blue;");
       this.#context = await new Module.Context();
 
       if (!this.#context) {
-        console.log('%c gPhoto2 Wasm Module Context Failed:', 'color: red;', 'context is:', this.#context);
+        console.log(
+          "%c gPhoto2 Wasm Module Context Failed:",
+          "color: red;",
+          "context is:",
+          this.#context,
+        );
       }
-      console.log('%c gPhoto2 Wasm Module Context Instantiated Successfully:', 'color: green;', this.#context);
-
+      console.log(
+        "%c gPhoto2 Wasm Module Context Instantiated Successfully:",
+        "color: green;",
+        this.#context,
+      );
     } catch (error) {
       this._state.next(CameraState.ERROR);
       throw error;
@@ -133,18 +153,17 @@ export class Camera {
 
   public async connect(): Promise<void> {
     if (!Camera.device || !Camera.device.opened) {
-      await this.pairCamera()
+      await this.pairCamera();
     }
 
     await this.loadWASM();
 
     if (Camera.device && this.#context) {
-      this._state.next(CameraState.CONNECTED)
-      console.log('%c camera connected successfully', 'color: green;');
-      this._state.next(CameraState.READY)
+      this._state.next(CameraState.CONNECTED);
+      console.log("%c camera connected successfully", "color: green;");
+      this._state.next(CameraState.READY);
     }
   }
-
 
   // CAMERA OPERATIONS
 
@@ -157,7 +176,7 @@ export class Camera {
 
     this.#queue = Promise.resolve();
 
-    console.log('%c preview started', 'color: cyan;');
+    console.log("%c preview started", "color: cyan;");
 
     this.previewActive = true;
     this._state.next(CameraState.BUSY);
@@ -167,13 +186,13 @@ export class Camera {
   }
 
   public async stopPreview(): Promise<void> {
-    if (!this.previewActive) return
+    if (!this.previewActive) return;
 
     this.previewActive = false;
     this.previewStream = null;
     this._state.next(CameraState.READY);
     await this.cancelCurrentOperation();
-    console.log('%c preview stopped', 'color: cyan;');
+    console.log("%c preview stopped", "color: cyan;");
   }
 
   private async streamFrames(onFrame: (blob: Blob) => void) {
@@ -181,10 +200,10 @@ export class Camera {
       try {
         const blob = await this.capturePreviewAsBlob();
         onFrame(blob);
-        await new Promise(resolve => requestAnimationFrame(resolve));
+        await new Promise((resolve) => requestAnimationFrame(resolve));
       } catch (error: any) {
-        console.error('error capturing preview as blob:', error);
-        if (error.message !== 'Camera is not ready') {
+        console.error("error capturing preview as blob:", error);
+        if (error.message !== "Camera is not ready") {
           this.previewActive = false;
           throw error;
         }
@@ -193,18 +212,20 @@ export class Camera {
   }
 
   private async capturePreviewAsBlob() {
-    return this.#schedule(context => context.capturePreviewAsBlob());
+    return this.#schedule((context) => context.capturePreviewAsBlob());
   }
 
   // CAPTURE MODE
   async captureImage(): Promise<File> {
     if (this.previewActive) await this.stopPreview();
 
-    console.log('%c capturing image', 'color: cyan;');
+    console.log("%c capturing image", "color: cyan;");
 
     this._state.next(CameraState.BUSY);
     try {
-      const file = await this.#schedule(context => context.captureImageAsFile());
+      const file = await this.#schedule((context) =>
+        context.captureImageAsFile(),
+      );
       return file;
     } catch (error) {
       this._state.next(CameraState.ERROR);
@@ -215,9 +236,8 @@ export class Camera {
   }
 
   async captureImageAsFile() {
-    return this.#schedule(context => context.captureImageAsFile());
+    return this.#schedule((context) => context.captureImageAsFile());
   }
-
 
   // DRIVER OPERATIONS
 
@@ -236,24 +256,29 @@ export class Camera {
   async cancelCurrentOperation() {
     const had_events = await this.consumeEvents();
     if (had_events) {
-      console.log('%c events consumed', 'color: yellow;');
+      console.log("%c events consumed", "color: yellow;");
     } else {
-      console.log('%c no events to consume', 'color: blue;');
+      console.log("%c no events to consume", "color: blue;");
     }
     this.#queue = Promise.resolve();
   }
 
   async disconnect() {
-    this._state.next(CameraState.DISCONNECTED);
     if (this.#context && !this.#context.isDeleted()) {
       this.#context.delete();
     }
+
     this.#context = null;
     this.#queue = Promise.resolve();
 
-    await Camera.device?.reset();
+    if (Camera.device?.opened) {
+      await Camera.device?.reset();
+      await Camera.device?.close();
+      await Camera.device?.releaseInterface(0);
+    }
 
-    console.log('%c webgphoto deinstantiated successfully', 'color: green;');
+    this._state.next(CameraState.DISCONNECTED);
+
+    console.log("%c webgphoto deinstantiated successfully", "color: green;");
   }
-
 }
