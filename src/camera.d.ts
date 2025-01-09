5fbbef3e4aeeb3c4c1870ba7075f242e5db15452
DahTach <francesco.tacinelli@gmail.com>
2025-01-09 16:10:46 +0100

    fix(forgot to update Camera.connect() type)

--- src/camera.d.ts
+++ src/camera.d.ts
@@ -1,17 +1,17 @@
-export type { Config, SupportedOps } from './libapi';
-import { Observable } from 'rxjs';
+export type { Config, SupportedOps } from "./libapi";
+import { Observable } from "rxjs";
 export declare function rethrowIfCritical(err: any): void;
 export enum CameraState {
   BUSY,
   READY,
   CONNECTED,
   DISCONNECTED,
-  ERROR
+  ERROR,
 }
 export declare class Camera {
   #private;
   state: Observable<CameraState>;
-  connect(): Promise<void>;
+  connect(): Promise<boolean>;
   disconnect(): Promise<void>;
   startPreview(onFrame: (blob: Blob) => void): Promise<void>;
   stopPreview(): Promise<void>;
